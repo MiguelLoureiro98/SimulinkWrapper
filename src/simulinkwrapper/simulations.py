b5eb484846c34d0ff37d1f75607549832f7116b2
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-12-03 12:47:23 +0000

    Added noise function. Started writing simulation classes.

---
+++ src/simulinkwrapper/simulations.py
@@ -0,0 +1,39 @@
+#   Copyright 2024 Miguel Loureiro
+
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+
+#       http://www.apache.org/licenses/LICENSE-2.0
+
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+This module contains classes to help users simulate dynamical systems modelled in Simulink.
+
+Classes
+-------
+Sim
+    Run a simulation in Simulink.
+
+RLSim
+    Run a simulation in Simulink as if it were an environment for Reinforcement Learning agents.
+"""
+
+import numpy as np
+
+class Sim(object):
+
+    def __init__(self) -> None:
+
+        return;
+
+class RLSim(object):
+
+    def __init__(self) -> None:
+
+        return;
