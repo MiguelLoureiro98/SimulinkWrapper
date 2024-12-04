ad41e904e0edd33bad83299195b49dceb16e8a7a
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-12-04 16:38:39 +0000

    Added validation data from Matlab/Simulink.

--- tests/integration/test_sim.py
+++ tests/integration/test_sim.py
@@ -3,3 +3,19 @@ This file contains tests targetting the simulations.py module.
 """
 
 import pytest
+
+if __name__ == "__main__":
+
+    from simulinkwrapper.simulations import Sim
+
+    model = "../_simulink/WT_pitch_actuator_no_sat";
+    vars = ["beta"];
+    ctrl = ["u"];
+
+    sim = Sim(model, vars, ctrl);
+
+    print("Connected successfully...");
+
+    sim.disconnect();
+
+    print("Disconnected successfully...");
