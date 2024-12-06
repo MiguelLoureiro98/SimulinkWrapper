05f41a5264e674f0a47f60f8dcf3d104e721dea2
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2024-12-06 18:09:50 +0000

    Added more test data.

--- tests/integration/test_sim.py
+++ tests/integration/test_sim.py
@@ -2,9 +2,48 @@
 This file contains tests targetting the simulations.py module.
 """
 
-import pytest
 import numpy as np
 import pandas as pd
+from simulinkwrapper.simulations import Sim
+from simulinkwrapper.noise import gen_noise_signal
+
+class controller_lookahead(object):
+
+    def __init__(self):
+
+        return;
+
+    def __call__(self, inputs: np.ndarray, refs: np.ndarray) -> np.ndarray:
+
+        return np.array([refs.sum()]);
+
+def test_var_step(): # needs a new model
+
+    pass
+
+def test_fixed_step():
+
+    pass
+
+def test_SISO():
+
+    pass
+
+def test_MIMO(): # needs a new model
+
+    pass
+
+def test_observer():
+
+    pass
+
+def test_varying_params():
+
+    pass
+
+def test_lookahead(): # control action will be 5
+
+    pass
 
 if __name__ == "__main__":
 
@@ -14,14 +53,14 @@ if __name__ == "__main__":
     model_name = "WT_pitch_actuator_no_sat";
     vars = ["beta"];
     ctrl = ["u"];
-    ref = np.ones(shape=(1, 10001));
+    ref = np.ones(shape=(1, 801));
 
-    sim = Sim(model_name, model_path, vars, ctrl, reference_signals=ref, stop_time=10.0);
+    sim = Sim(model_name, model_path, vars, ctrl, reference_signals=ref, stop_time=0.8, solver_type="fixed_step");
 
     print("Connected successfully...");
 
     results = sim.run();
-    sim.plot("beta");
+    sim.plot("beta", 0);
 
     sim.disconnect();
 
